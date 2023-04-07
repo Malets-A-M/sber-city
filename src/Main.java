d16b7f4677c53d4ff9f669c45b236b3f258b3fc0
Mslets-A-M <andrej160195@mail.ru>
2023-04-07 14:26:41 +0300

    Add searching population

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,16 @@ public class Main {
             if (city.length == 6) cities.add(new City(city[1], city[2], city[3], Integer.parseInt(city[4]), city[5]));
             else cities.add(new City(city[1], city[2], city[3], Integer.parseInt(city[4]), null));
         }
-        System.out.println(cities);
+
+        int index = 0;
+        int population = 0;
+        City [] arrayCities = cities.toArray(new City[0]);
+        for (var i = 0; i < arrayCities.length; i++) {
+            if (population < arrayCities[i].getPopulation()) {
+                index = i;
+                population = arrayCities[i].getPopulation();
+            }
+        }
+        System.out.println("[" + index + "] = " + population);
     }
 }
