8224cf81958f85ddaba31f05dd526a5c38b15160
Salam4ik <andrej160195@mail.ru>
2023-04-07 15:01:31 +0300

    Add number of cities in the region

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,29 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        List <City> cities = new ArrayList<>();
-        String [] city;
+        List<City> cities = new ArrayList<>();
+        String[] city;
         Scanner scanner = new Scanner(new File("Задача ВС Java Сбер.csv"));
         while (scanner.hasNext()) {
             city = scanner.nextLine().split(";");
             if (city.length == 6) cities.add(new City(city[1], city[2], city[3], Integer.parseInt(city[4]), city[5]));
             else cities.add(new City(city[1], city[2], city[3], Integer.parseInt(city[4]), null));
         }
-        System.out.println(cities);
+
+
+        Map<String, Integer> amountCities = new HashMap<>();
+        for (var cityOfCities : cities) {
+            if (!amountCities.containsKey(cityOfCities.getRegion())) {
+                amountCities.put(cityOfCities.getRegion(), 1);
+            } else {
+                amountCities.put(cityOfCities.getRegion(), amountCities.remove(cityOfCities.getRegion()) + 1);
+            }
+        }
+        for (var key : amountCities.keySet()) {
+            System.out.println(key + " - " + amountCities.get(key));
+        }
     }
 }
