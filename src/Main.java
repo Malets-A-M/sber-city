ec87b05663221c303ba673e9ade116fbd17498c6
Salam4ik <andrej160195@mail.ru>
2023-04-07 11:56:23 +0300

    Add sorting

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,6 +15,20 @@ public class Main {
             if (city.length == 6) cities.add(new City(city[1], city[2], city[3], Integer.parseInt(city[4]), city[5]));
             else cities.add(new City(city[1], city[2], city[3], Integer.parseInt(city[4]), null));
         }
-        System.out.println(cities);
+
+        cities.sort(Comparator.comparing(City::getName));
+        System.out.println("\n----------< Сортировка списка городов по наименованию >----------\n" +
+                cities + "\n=================================================================\n");
+
+        cities.sort((o1, o2) -> {
+            if (o1.getDistrict().equals(o2.getDistrict())) {
+                return o1.getName().compareTo(o2.getName());
+            } else {
+                return o1.getDistrict().compareTo(o2.getDistrict());
+            }
+        });
+
+        System.out.println("\n----------< Сортировка списка городов по федеральному округу и наименованию города внутри каждого федерального округа >----------\n" +
+                cities + "\n=================================================================\n");
     }
 }
